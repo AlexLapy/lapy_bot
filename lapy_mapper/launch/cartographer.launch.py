2813137fa9a82548120eebca5736f839f94d9e8b
AlexLapy <alexlapyy@gmail.com>
2023-09-04 12:11:30 -0400

    Added lapy_mapper with carto

---
+++ lapy_mapper/launch/cartographer.launch.py
@@ -0,0 +1,38 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    package_name='lapy_mapper'
+
+    cartographer_config_dir = os.path.join(get_package_share_directory(package_name), 'config')
+    configuration_basename = "cartographer.lua"
+
+
+    occu_grid_node = Node(
+        package='cartographer_ros',
+        executable='occupancy_grid_node',
+        output='screen',
+        name='occupancy_grid_node',
+        parameters=[{'use_sim_time': True}],
+        arguments=['-resolution', '0.05', '-publish_period_sec', '1.0'])
+
+    cartographer_node = Node(            
+        package='cartographer_ros', 
+        executable='cartographer_node', 
+        name='cartographer_node',
+        output='screen',
+        parameters=[{'use_sim_time': True}],
+        arguments=['-configuration_directory', cartographer_config_dir,
+                   '-configuration_basename', configuration_basename] 
+        )
+
+
+    return LaunchDescription([
+        cartographer_node,
+        occu_grid_node,
+    ])
